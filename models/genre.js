a7bab5d34711d7e689cdd036ef5ae51e301df2e5
Lawrence Amoafo <rekenburg777@gmail.com>
2023-11-30 10:30:04 +0000

    Create one model per file

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+});
+
+// Virtual for genre's URL
+GenreSchema.virtual('url').get(function () {
+  return `/catalog/genre/${this._id}`;
+});
+
+module.exports = mongoose.model('Genre', GenreSchema);
