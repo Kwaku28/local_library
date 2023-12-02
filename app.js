b210ca578ee13440fe76af60a3cefd2fdda72f90
Lawrence Amoafo <rekenburg777@gmail.com>
2023-12-02 20:19:02 +0000

    Import routes for "catalog" area of site

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
