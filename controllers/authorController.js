f168cb77529bc82f81a5445489d800d4e5d6a97d
Lawrence Amoafo <rekenburg777@gmail.com>
2023-12-07 03:19:49 +0000

    Implement function to list all authors

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,7 +3,12 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author list");
+  const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
+
+  res.render("author_list", {
+    title: "Author List",
+    author_list: allAuthors,
+  });
 });
 
 // Display detail page for a specific Author.
