d08952d407e793961c82abaaed54b567ba0cbad8
Lawrence Amoafo <rekenburg777@gmail.com>
2023-12-07 03:20:23 +0000

    Format date of birth and deaths using luxon

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -24,6 +25,23 @@ AuthorSchema.virtual("name").get(function () {
 // Virtual for author's URL
 AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+
+  return lifetime_string;
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
