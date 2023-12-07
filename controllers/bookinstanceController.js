3813d7950d4b56f759f887bae17601245cdda56f
Lawrence Amoafo <rekenburg777@gmail.com>
2023-12-07 03:17:30 +0000

    Implement function to fetch all bookinstances

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -3,7 +3,12 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all BookInstances.
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance list");
+  const allBookInstances = await BookInstance.find().populate("book").exec();
+
+  res.render("bookinstance_list", {
+    title: "Book Instance List",
+    bookinstance_list: allBookInstances,
+  });
 });
 
 // Display detail page for a specific BookInstance.
