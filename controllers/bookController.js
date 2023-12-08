ad8d37919d8e952182222f835dc79c043b7cbd8f
Lawrence Amoafo <rekenburg777@gmail.com>
2023-12-08 10:49:30 +0000

    Implement function to fetch book details and instances for a specific book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -43,7 +43,24 @@ exports.book_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific book.
 exports.book_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+  // Get details of books, book instances for specific book
+  const [book, bookInstances] = await Promise.all([
+    Book.findById(req.params.id).populate("author").populate("genre").exec(),
+    BookInstance.find({ book: req.params.id }).exec(),
+  ]);
+
+  if (book === null) {
+    // No results.
+    const err = new Error("Book not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("book_detail", {
+    title: book.title,
+    book: book,
+    book_instances: bookInstances,
+  });
 });
 
 // Display book create form on GET.
