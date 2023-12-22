1139e3435e874fcfc555a532a4219b6b716b6a1d
Lawrence Amoafo <rekenburg777@gmail.com>
2023-12-22 14:59:19 +0000

    Install and use production ready dependencies

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
+
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -29,10 +37,19 @@ async function main() {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(limiter);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
